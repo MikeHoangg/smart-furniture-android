07339f2c57ba0169410a790368d9f5e2154543db
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-12-26 23:04:24 +0200

    refactored login activity, added user class, added retrofit package for requests to api

---
+++ app/src/main/java/mikehoang/smartfurniture/User.java
@@ -0,0 +1,31 @@
+package mikehoang.smartfurniture;
+
+public class User {
+    private String email;
+    private String username;
+    private double height;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
